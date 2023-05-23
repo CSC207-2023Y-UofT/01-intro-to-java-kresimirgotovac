6ae7e25ef1015463822f49a39aad2f1c288ca6e3
kresimirgotovac <kresimir.gotovac@mail.utoronto.ca>
2023-05-23 16:52:27 -0400

    quiz 1

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -7,6 +7,9 @@
  * relevant readings for each task.
  */
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 /**
  *  1. In Java, no code can exist outside of a class. Unlike Python which uses
  *    functions, all code in Java uses methods.
@@ -43,7 +46,7 @@ public class Basics {
         /* TODO (Task 1): Write a line of code below that prints the string
          *                Hello World!
          */
-
+        System.out.println("Hello World!");
 
 
         /* 4. In Python, we could declare variables using a simple assignment
@@ -62,7 +65,7 @@ public class Basics {
          * TODO (Task 2): Create a variable named my_variable and assign it the
          *                value 100.
          */
-
+        int my_variable = 100;
 
 
         /* Do not remove the line below: if you did task 2 correctly, then
@@ -99,7 +102,9 @@ public class Basics {
          * Current count: 1
          * Current count: 0
          */
-
+        for (int i = 10; i >= 0; i--){
+            System.out.println("Current count: " + i);
+        }
 
     }
 
@@ -141,6 +146,10 @@ public class Basics {
          *                you may use them if you wish.
          */
         StringBuilder ret = new StringBuilder();
+        String[] strings = to_split.split(" ", 7);
+        for (int i = 0; i <= 6; i++){
+            ret.append(strings[i].charAt(0));
+        }
 
         // Fill in the rest of the body here
 
@@ -170,7 +179,15 @@ public class Basics {
          *                You can index into arrays as we do in Python
          *                (e.g. arr[i] gives you the item at index i).
          */
-
+        int k = arr.length;
+        if (k <= 1){
+            return 0;
+        }
+        for (int i = 0; i < k; i++){
+            if (i % 2 != 0) {
+                current_sum += arr[i];
+            }
+        }
         return current_sum;
     }
 
